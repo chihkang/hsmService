a8dbe0f622aa65c09c0115e67ca21dc5b2be9b46
ChihKang <chihkang@me.com>
2017-07-26 23:51:55 +0800

    Init HsmService

---
+++ HsmWebServiceUsingDB/Controllers/HsmTransactionsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using HsmWebServiceUsingDB.Models;
+
+namespace HsmWebServiceUsingDB.Controllers
+{
+    public class HsmTransactionsController : ApiController
+    {
+        private HsmServiceDBContext db = new HsmServiceDBContext();
+
+        // GET: api/HsmTransactions
+        public IQueryable<HsmTransaction> GetHsmTransactions()
+        {
+            return db.HsmTransactions;
+        }
+
+        // GET: api/HsmTransactions/5
+        [ResponseType(typeof(HsmTransaction))]
+        public async Task<IHttpActionResult> GetHsmTransaction(Guid id)
+        {
+            HsmTransaction hsmTransaction = await db.HsmTransactions.FindAsync(id);
+            if (hsmTransaction == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(hsmTransaction);
+        }
+
+        // PUT: api/HsmTransactions/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutHsmTransaction(Guid id, HsmTransaction hsmTransaction)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != hsmTransaction.TransactionSeq)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(hsmTransaction).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HsmTransactionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/HsmTransactions
+        [ResponseType(typeof(HsmTransaction))]
+        public async Task<IHttpActionResult> PostHsmTransaction(HsmTransaction hsmTransaction)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.HsmTransactions.Add(hsmTransaction);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (HsmTransactionExists(hsmTransaction.TransactionSeq))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = hsmTransaction.TransactionSeq }, hsmTransaction);
+        }
+
+        // DELETE: api/HsmTransactions/5
+        [ResponseType(typeof(HsmTransaction))]
+        public async Task<IHttpActionResult> DeleteHsmTransaction(Guid id)
+        {
+            HsmTransaction hsmTransaction = await db.HsmTransactions.FindAsync(id);
+            if (hsmTransaction == null)
+            {
+                return NotFound();
+            }
+
+            db.HsmTransactions.Remove(hsmTransaction);
+            await db.SaveChangesAsync();
+
+            return Ok(hsmTransaction);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool HsmTransactionExists(Guid id)
+        {
+            return db.HsmTransactions.Count(e => e.TransactionSeq == id) > 0;
+        }
+    }
+}
---
+++ HsmWebServiceUsingDB/Models/HsmTransaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HsmWebServiceUsingDB.Models
+{
+    public class HsmTransaction
+    {
+        public DateTime InTime { get; set; }
+        public DateTime OutTime { get; set; }
+        [Key]
+        public Guid TransactionSeq { get; set; }
+        public string ApplicationID { get; set; }
+        public string ApplicationData { get; set; }
+        public string rtnCode { get; set; }
+        public string MethodName { get; set; }
+    }
+}
